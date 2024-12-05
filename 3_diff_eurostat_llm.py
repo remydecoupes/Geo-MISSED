e9ee831796ba7782087781e148720b6376bc1e3c
Ubuntu <remy.decoupes@gmail.com>
2024-12-05 21:38:10 +0000

    start gh repo

---
+++ 3_diff_eurostat_llm.py
@@ -0,0 +1,85 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default="llama3:8b")
+args = parser.parse_args()
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from shapely import wkt 
+import geopandas as gpd
+import folium
+from branca.colormap import LinearColormap
+import numpy as np
+
+if args.model == "codestral": 
+    model_short_name = "codestral"
+elif args.model == "llama3:8b":
+    model_short_name = "llama3-8b"
+elif args.model == "qwen2.5:7b":
+    model_short_name = "qwen2.5-7b"
+
+year = 2017
+NUTS_Level = 2
+
+df = pd.read_csv(f'./output/gdp_{year}_nuts_{NUTS_Level}_llm_{model_short_name}.csv')
+
+try :
+    df['geometry'] = df['geometry'].apply(wkt.loads)
+except:
+    print("geometry loading wkt: already done")
+gdf = gpd.GeoDataFrame(df, geometry='geometry')
+if gdf.crs is None:
+    gdf = gdf.set_crs(epsg=3035)  # Ajustez si nécessaire (EPSG initial probable)
+gdf = gdf.to_crs(epsg=4326)
+
+gdf = gdf[gdf["LEVL_CODE"] == NUTS_Level]
+gdf['2017_predicted'] = gdf['2017_predicted'].round(0)
+gdf['diff_eurostat_llm'] = ((gdf['2017_predicted'] - gdf['2017'])).round(0)
+gdf.loc[gdf['diff_eurostat_llm'] > 100000, 'diff_eurostat_llm'] = np.nan
+gdf['diff_eurostat_llm_normalized'] = gdf['diff_eurostat_llm'] / gdf['2017']
+
+
+gdf[f"{year}_deviation"] = gdf[f"{year}_deviation"].round(0)
+
+gdf_json = gdf.to_crs(epsg=4326).to_json()
+
+m = folium.Map(location=[50, 20], zoom_start=5)
+folium.Choropleth(
+    geo_data=gdf_json,
+    data=gdf,  
+    # columns=['NUTS_ID', 'diff_eurostat_llm'], 
+    columns=['NUTS_ID', 'diff_eurostat_llm_normalized'], 
+    key_on='feature.properties.NUTS_ID',  
+    fill_color='RdBu',  
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name=f'Difference between eurostat {year} and {model_short_name}'
+).add_to(m)
+
+style_function = lambda x: {'fillColor': '#ffffff', 
+                            'color':'#000000', 
+                            'fillOpacity': 0.1, 
+                            'weight': 0.1}
+highlight_function = lambda x: {'fillColor': '#000000', 
+                                'color':'#000000', 
+                                'fillOpacity': 0.50, 
+                                'weight': 0.1}
+
+hover = folium.features.GeoJson(
+    gdf,
+    style_function=style_function, 
+    control=False,
+    highlight_function=highlight_function, 
+    tooltip=folium.features.GeoJsonTooltip(
+        fields=['NUTS_NAME', str(year), f"{year}_predicted", "diff_eurostat_llm_normalized", f"{year}_deviation"],
+        aliases=["Region: ", "Eurostat GDP: ", "LLM predicted: ", "normalized diff: ", "std deviation for 3 llm prediction: "],
+        style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
+    )
+)
+m.add_child(hover)
+m.keep_in_front(m)
+
+
+m.save(f'./output/gdp_{year}_nuts_{NUTS_Level}_llm_{model_short_name}.html')
+m.save(f'./docs/gdp_{year}_nuts_{NUTS_Level}_llm_{model_short_name}.html')
---
+++ README.md
@@ -0,0 +1,32 @@
+# Explore LLMs geographic bias for European Country
+
+The aim of this work is to map geographic bias normalized by GDP indicator 
+
+## Data
+
+| Title | link | metadata | name file |
+|---|---|---|---|
+| GDP at current market prices by NUTS 3 regions| [link](https://ec.europa.eu/eurostat/web/main/data/database) | [metadata](https://ec.europa.eu/eurostat/cache/metadata/en/reg_eco10_esms.htm) | [estat_nama_10r_3gdp.tsv](./data/estat_nama_10r_3gdp.tsv) |
+| NUTS3 region| | | [NUTS_RG_01M_2024_3035.geojson](./data/NUTS_RG_01M_2024_3035.geojson)|
+
+## Install environment
+
+**Code**:
+
+```bash
+conda create -n geobias python=3.10 pip ipython
+conda activate geobias 
+pip install geopandas pandas folium langchain langchain_community langchain_core timeout_decorator langchain_openai matplotlib pycountry
+```
+
+**Data**: 
+
+You have to donwload the data files into data folder
+
+-------
+<img align="left" src="https://www.umr-tetis.fr/images/logo-header-tetis.png">
+
+
+|           |
+|----------------------|
+| Rémy Decoupes        |
