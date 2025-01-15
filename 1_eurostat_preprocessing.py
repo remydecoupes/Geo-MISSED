99aaded1afffec33e7f45ad2d2669092470d61a5
Ubuntu <remy.decoupes@gmail.com>
2025-01-15 18:44:08 +0000

    prepare for relative prediction

--- 1_eurostat_preprocessing.py
+++ 1_eurostat_preprocessing.py
@@ -40,6 +40,14 @@ eurostat_data_files = [
         "unit": "NR"
     }
 ]
+
+def get_average_country_indicator(name):
+    try:
+        average_country_indicator = int(merged_gdf[merged_gdf["CNTR_CODE"] == name]['data'].mean().round(0))
+    except:
+        average_country_indicator = np.nan
+    return average_country_indicator
+
 for eurostat_dict in eurostat_data_files:
     indicator = eurostat_dict["indicator"]
     path = eurostat_dict["path"]
@@ -52,6 +60,7 @@ for eurostat_dict in eurostat_data_files:
     
     df_eurostat = pd.read_csv(path.replace(data_dir, output_dir), sep="\t")
     df_eurostat.rename(columns={"geo\\TIME_PERIOD": "NUTS_ID"}, inplace=True)
+
     # Correct column name because there are space " " in name
     for col_year in range(2015, 2023):
         try:
@@ -68,8 +77,32 @@ for eurostat_dict in eurostat_data_files:
     df_eurostat["data"] = df_eurostat["data"].replace(":", np.nan)
     df_eurostat = df_eurostat[df_eurostat['unit'] == eurostat_dict["unit"]]
 
+    if indicator == "age_index": # compute ratio
+        # df_eurostat = df_eurostat[df_eurostat["age"].isin(["Total", "Y_LT15", "Y65-84", "Y_GE85"])]
+        df_Y_LT15 = df_eurostat[df_eurostat["age"] == "Y_LT15"].reset_index()
+        df_Y_65_84 = df_eurostat[df_eurostat["age"] == "Y65-84"].reset_index()
+        df_Y_GE85 = df_eurostat[df_eurostat["age"] == "Y_GE85"].reset_index()
+        list_nuts_with_stat = list(df_Y_LT15["NUTS_ID"].values)
+        df_total = df_eurostat[df_eurostat["age"] == "TOTAL"]
+        df_total = df_total[df_total["NUTS_ID"].isin(list_nuts_with_stat)].reset_index()
+        df_result = df_total[["NUTS_ID"]].reset_index()
+        df_result["age_risk_percent"] = (df_Y_LT15["data"] + df_Y_65_84["data"] + df_Y_GE85["data"]) / df_total["data"] * 100
+        df_eurostat = df_result
+        df_eurostat["data"] = df_eurostat["age_risk_percent"]
+        df_eurostat[eurostat_dict["complete_year"]] = df_eurostat["age_risk_percent"]
+        df_eurostat["unit"] = "NR"
+
+
     print(indicator)
     print(df_eurostat[["NUTS_ID", "unit", "data", eurostat_dict["complete_year"]]])
 
     merged_gdf = gdf_nuts3.merge(df_eurostat, left_on="NUTS_ID", right_on="NUTS_ID", how="inner")
-    merged_gdf[["NUTS_ID", "unit", "data", eurostat_dict["complete_year"]]].to_csv(f'./output/eurostat_{indicator}_{eurostat_dict["complete_year"]}.csv')
+    df_country_level = merged_gdf[merged_gdf["LEVL_CODE"] == 0]
+    df_country_level["average_country_indicator"] = merged_gdf["CNTR_CODE"].apply(get_average_country_indicator)
+    df_country_level = df_country_level[["CNTR_CODE", "average_country_indicator"]]
+    print(f"nb of countries: {len(df_country_level)}")
+    merged_gdf = merged_gdf.merge(df_country_level, left_on="CNTR_CODE", right_on="CNTR_CODE", how="inner")
+    merged_gdf[f"relative_{indicator}"] = merged_gdf[f"data"] - merged_gdf["average_country_indicator"]
+
+
+    merged_gdf[["CNTR_CODE", "NUTS_NAME", "NUTS_ID", "LEVL_CODE", "unit", "data", eurostat_dict["complete_year"], "average_country_indicator", f"relative_{indicator}", "geometry"]].to_csv(f'./output/eurostat_{indicator}_{eurostat_dict["complete_year"]}.csv')
--- 2_llm_prediction_transformers.py
+++ 2_llm_prediction_transformers.py
@@ -1,4 +1,4 @@
-
+import argparse
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import torch.nn.functional as F
@@ -7,31 +7,102 @@ import pandas as pd
 import pycountry
 import statistics
 from tqdm import tqdm
+import re
+
 tqdm.pandas()
 
-# Model and tokenizer setup
-MODEL = "Qwen/Qwen2.5-7B-Instruct" 
-model_short_name = "Qwen2.5-7B-Instruct"
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
-# Load model with INT8 quantization
-model = AutoModelForCausalLM.from_pretrained(
-    MODEL,
-    device_map="auto",
-    torch_dtype=torch.float16,
-    load_in_8bit=True,  # Enable 8-bit quantization
-)
+data_dir = "./output"
+eurostat_data_files = [
+    {
+        "indicator": "income",
+        "path": f"{data_dir}/eurostat_income_2017.csv",
+        "complete_year": "2017",
+        "unit": "EUR_HAB"
+    },
+    {
+        "indicator": "pop_density",
+        "path": f"{data_dir}/eurostat_pop_density_2018.csv",
+        "complete_year": "2018",
+        "unit": "PER_KM2",
+        "nan": ":", # null value are ":" and not empty cell
+    },
+    {
+        "indicator": "poverty",
+        "path": f"{data_dir}/eurostat_poverty_2022.csv",
+        "complete_year": "2022",
+        "unit": "PC",
+        "nan": ":"
+    },
+    {
+        "indicator": "age_index",
+        "path": f"{data_dir}/eurostat_age_index_2021.csv",
+        "complete_year": "2021",
+        "unit": "NR"
+    }
+]
 
-year = 2017
-
-def prediction(NUTS_ID, country):
+def prediction(NUTS_ID, country, indicator, year):
     NUTS_ID = NUTS_ID
     country = country
-    prompt = f"What is the average income per inhabitant for {NUTS_ID} in {country} in {year}?"
-    messages = [
-        {"role": "system", "content": f'You are a statistician working for the European commission at EUROSTAT. You have to give the average income per inhabitant by NUTS 1 or 2 or 3 level for year {year}. Don\'t compute it, just guess the income.Answer only with the average income (a single number) without any other words or repetition of the question. Don\'t repeat the prompt neither. Example of answer: "30000".'},
-        {"role": "user", "content": prompt}
-    ]
+    if indicator == "income":
+        prompt = f"What is the average income per inhabitant for {NUTS_ID} in {country} in {year}?"
+        messages = [
+            {"role": "system", 
+             "content": (
+                 f'You are a statistician working for the European commission at EUROSTAT.'
+                 f'You have to give the average income per inhabitant by NUTS 1 or 2 or 3 level for year {year}.'
+                 f"Don\'t compute it, just guess the income. Answer only with the average income (a single number)"
+                 f"without any other words or repetition of the question. Don\'t repeat the prompt neither. Example of answer: '30000'."
+                )
+            },
+            {"role": "user", "content": prompt}
+        ]
+    elif indicator == "pop_density":
+        prompt = f"What is the population density (in PER/km2) for {NUTS_ID} in {country} in {year}?"
+        messages = [
+            {"role": "system",
+             "content": (
+                 f"You are a statistician working for the European commission at EUROSTAT. "
+                 f"You have to give the population density (nb the person per km2) by NUTS 1 or 2 or 3 level for year {year}. "
+                 f"Don't compute it, just guess the density. Answer only with the density (a single number) "
+                 f"without any other words or repetition of the question. Don't repeat the prompt neither. "
+                 f"Example of answer: '146.3'."
+                )
+            },
+            {"role": "user", "content": prompt}
+        ]
+    elif indicator == "poverty":
+        prompt = f"What is the percent of the population considered as poor by Eurostat for region: {NUTS_ID} in {country} in {year}?"
+        messages = [
+            {"role": "system",
+             "content": (
+                 f"You are a statistician working for the European commission at EUROSTAT. "
+                 f"You have to give the percent à poop people by NUTS 1 or 2 or 3 level for year {year}. "
+                 f"Don't compute it, just guess the ratio. Answer only with the ratio (a single number) "
+                 f"without any other words or repetition of the question. Don't repeat the prompt neither. "
+                 f"Example of answer: '25.3'."
+                )
+            },
+            {"role": "user", "content": prompt}
+        ]
+    elif indicator == "age_index":
+        prompt = f"What is the percent of the population under 15 years old or older than 65 years old for region: {NUTS_ID} in {country} in {year}?"
+        messages = [
+            {"role": "system",
+             "content": (
+                 f"You are a statistician working for the European commission at EUROSTAT. "
+                 f"You have to give the percent of young (<15year old) + old (>65 years) people by NUTS 1 or 2 or 3 level for year {year}. "
+                 f"Don't compute it, just guess the ratio. Answer only with the ratio (a single number) "
+                 f"without any other words or repetition of the question. Don't repeat the prompt neither. "
+                 f"Example of answer: '25.3'."
+                )
+            },
+            {"role": "user", "content": prompt}
+        ]
+    else:
+        print("wrong indicator !!")
+
     # Tokenize input prompt
     inputs = tokenizer.apply_chat_template(
         messages, 
@@ -67,16 +138,72 @@ def prediction(NUTS_ID, country):
 
     # Decode the generated response
     generated_text = tokenizer.decode(generated_tokens[len(inputs.input_ids[0]):], skip_special_tokens=True)
+    generated_text = re.sub(r"[^\d.]", "", generated_text)
     
     #return generated_text, generated_token_ids, average_token_logprobs
     return generated_text, average_token_logprobs
 
-def relative_prediction(NUTS_ID, country, country_income):
-    prompt = f"What is the difference of income per inhabitant between {NUTS_ID} ({country}) and {country} (country income: {country_income})  in {year}?"
+def relative_prediction(NUTS_ID, country, country_indicator, indicator, year):
+    prompt = f"What is the difference of income per inhabitant between {NUTS_ID} ({country}) and {country} (country average {indicator}: {country_indicator})  in {year}?"
     messages = [
         {"role": "system", "content": f'You are a statistician working for the European commission at EUROSTAT. You have to give the difference of average income per inhabitant between a sub regions at NUTS 1 or 2 or 3 level and its country for {year}. Don\'t compute it, just guess the diffrence of income. Answer only with the difference income (a single number) without any other words or repetition of the question. Don\'t repeat the prompt neither. Example of answer: 3000.'},
         {"role": "user", "content": prompt}
     ]
+    if indicator == "income":
+        prompt = f"What is the difference of income per inhabitant between {NUTS_ID} ({country}) and {country} (country average {indicator}: {country_indicator})  in {year}?"
+        messages = [
+            {"role": "system", 
+             "content": (
+                 f'You are a statistician working for the European commission at EUROSTAT.'
+                 f'You have to give the difference of average income per inhabitant between a sub regions at NUTS 1 or 2 or 3 level and its country for {year}'
+                 f"Don\'t compute it, just guess the diffrence of income. Answer only with the difference income (a single number) without any other words or repetition of the question"
+                 f"without any other words or repetition of the question. Don\'t repeat the prompt neither. Example of answer: 3000."
+                )
+            },
+            {"role": "user", "content": prompt}
+        ]
+    elif indicator == "pop_density":
+        prompt = f"What is the difference of population density (in PER/km2) between {NUTS_ID} ({country}) and {country} (country average {indicator}: {country_indicator})  in {year}?"
+        messages = [
+            {"role": "system", 
+             "content": (
+                 f'You are a statistician working for the European commission at EUROSTAT.'
+                 f'You have to give the difference of population density (in PER/km2) between a sub regions at NUTS 1 or 2 or 3 level and its country for {year}'
+                 f"Don\'t compute it, just guess the diffrence of population density (in PER/km2). Answer only with the difference population density (in PER/km2) (a single number) without any other words or repetition of the question"
+                 f"without any other words or repetition of the question. Don\'t repeat the prompt neither. Example of answer: 30."
+                )
+            },
+            {"role": "user", "content": prompt}
+        ]
+    elif indicator == "poverty":
+        prompt = f"What is the difference of percent of the population considered as poor between {NUTS_ID} ({country}) and {country} (country average {indicator}: {country_indicator})  in {year}?"
+        messages = [
+            {"role": "system", 
+             "content": (
+                 f'You are a statistician working for the European commission at EUROSTAT.'
+                 f'You have to give the difference of percent of the population considered as poor between a sub regions at NUTS 1 or 2 or 3 level and its country for {year}'
+                 f"Don\'t compute it, just guess the diffrence of percent of the population considered as poor. Answer only with the difference ratio (a single number) without any other words or repetition of the question"
+                 f"without any other words or repetition of the question. Don\'t repeat the prompt neither. Example of answer: 30."
+                )
+            },
+            {"role": "user", "content": prompt}
+        ]
+    elif indicator == "age_index":
+        prompt = f"What is the difference of percent of the population under 15 years old or older than 65 years old between {NUTS_ID} ({country}) and {country} (country average {indicator}: {country_indicator})  in {year}?"
+        messages = [
+            {"role": "system", 
+             "content": (
+                 f'You are a statistician working for the European commission at EUROSTAT.'
+                 f'You have to give the difference of percent of the population under 15 years old or older than 65 years old between a sub regions at NUTS 1 or 2 or 3 level and its country for {year}'
+                 f"Don\'t compute it, just guess the diffrence of ratio. Answer only with the difference ratio (a single number) without any other words or repetition of the question"
+                 f"without any other words or repetition of the question. Don\'t repeat the prompt neither. Example of answer: 30."
+                )
+            },
+            {"role": "user", "content": prompt}
+        ]
+    else:
+        print("wrong indicator !!")
+
     # Tokenize input prompt
     inputs = tokenizer.apply_chat_template(
         messages, 
@@ -117,7 +244,7 @@ def relative_prediction(NUTS_ID, country, country_income):
     return generated_text, average_token_logprobs
 
 
-def average_prediction(row):
+def average_prediction(row, indicator, year):
     """
     Compute the average and deviation of predictions for a given NUTS_ID and country.
     Expects the row to contain both 'NUTS_ID' and 'country'.
@@ -127,12 +254,12 @@ def average_prediction(row):
     logprobs = []
 
     for _ in range(number_prediction):
-        prediction_value, logprob = prediction(row["NUTS_NAME"], row["country"])
+        prediction_value, logprob = prediction(row["NUTS_NAME"], row["country"], indicator, year)
         predictions.append(prediction_value)
         logprobs.append(logprob)
 
     # Handle potential NaN values in predictions
-    predictions = [int(p) for p in predictions if not pd.isna(p)]
+    predictions = [float(p) for p in predictions if not pd.isna(p)]
     logprobs = [float(p) for p in logprobs if not pd.isna(p)]
     print(f'{row["NUTS_NAME"]}/{row["country"]}: {predictions} | {logprobs}')
 
@@ -151,7 +278,7 @@ def average_prediction(row):
 
     return average, deviation, logprobs_average, logprobs_deviation
 
-def average_relative_prediction(row):
+def average_relative_prediction(row, indicator, indicator_country, year):
     """
 .
     """
@@ -161,7 +288,7 @@ def average_relative_prediction(row):
 
     for _ in range(number_prediction):
         #relative_prediction(NUTS_ID, country, capital, capital_income):
-        prediction_value, logprob = relative_prediction(row["NUTS_NAME"], row["country"], row["country_income"],)
+        prediction_value, logprob = relative_prediction(row["NUTS_NAME"], row["country"], row["country_income"], indicator, year)
         predictions.append(prediction_value)
         logprobs.append(logprob)
 
@@ -192,21 +319,53 @@ def average_relative_prediction(row):
 if __name__ == "__main__":
     import multiprocessing
     multiprocessing.set_start_method("spawn", force=True)
-    # Example usage
-    # NUTS_ID = "Auvergne"
-    # country = "France"
-    # result = prediction(NUTS_ID, country)
-    # print("Final Prediction Result:", result)
 
-    df = pd.read_csv(f"./output/gdp_{year}.csv")
-    df["country"] = df["CNTR_CODE"].apply(lambda code: pycountry.countries.get(alpha_2=code).name if pycountry.countries.get(alpha_2=code) else "Unknown")
-
-    # df = df.iloc[0:100]
-
-    df[[f"{year}_predicted", f"{year}_deviation", f"{year}_logprobs", f"{year}_logprobs_deviation"]] = df.progress_apply(average_prediction, axis=1).apply(pd.Series)
-    df[[f"{year}_relative_predicted", f"{year}_relative_deviation", f"{year}_relative_logprobs", f"{year}_relative_logprobs_deviation"]] = df.progress_apply(average_relative_prediction, axis=1).apply(pd.Series)
-    
-    try :
-        df.to_csv(f'./output/gdp_{year}_nuts_llm_{model_short_name}.csv')
+    try:
+        parser = argparse.ArgumentParser(description="Load and configure a language model.")
+        parser.add_argument(
+            "--model",
+            type=str,
+            default="Qwen/Qwen2.5-7B-Instruct",
+            help="Name of the model to load. Default is 'Qwen/Qwen2.5-7B-Instruct'."
+        )
+        args = parser.parse_args()
+        MODEL = args.model
     except:
-        df.to_csv(f'./output/gdp_{year}_nuts_llm_error_on_name.csv')
+        MODEL = "Qwen/Qwen2.5-7B-Instruct"
+    model_short_name = MODEL.split('/')[-1]  # Extract short name from full model path
+
+    print(f"Loading model: {MODEL} ({model_short_name})")
+
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(MODEL)
+        model = AutoModelForCausalLM.from_pretrained(
+            MODEL,
+            device_map="auto",
+            torch_dtype=torch.float16,
+            load_in_8bit=True,  # Enable 8-bit quantization
+        )
+        print("Model and tokenizer loaded successfully.")
+    except Exception as e:
+        print(f"Error loading model: {e}")
+
+    for eurostat_dict in eurostat_data_files:
+        indicator = eurostat_dict["indicator"]
+        path = eurostat_dict["path"]
+        year = eurostat_dict["complete_year"]
+
+        print(f"Work on: {indicator}")
+        df = pd.read_csv(path)
+        df["country"] = df["CNTR_CODE"].apply(lambda code: pycountry.countries.get(alpha_2=code).name if pycountry.countries.get(alpha_2=code) else "Unknown")
+
+        # df = df.iloc[0:100]
+        # df[[f"{indicator}_predicted", f"{indicator}_deviation", f"{indicator}_logprobs", f"{indicator}_logprobs_deviation"]] = df.progress_apply(lambda row: average_prediction(row, indicator, year), axis=1).apply(pd.Series)
+        # try :
+        #     df.to_csv(f'./output/{indicator}_{year}_nuts_llm_{model_short_name}.csv')
+        # except:
+        #     df.to_csv(f'./output/{indicator}_{year}_nuts_llm_error_on_name.csv')
+
+        df[[f"{indicator}_relative_predicted", f"{indicator}_relative_deviation", f"{indicator}_relative_logprobs", f"{indicator}_relative_logprobs_deviation"]] = df.progress_apply(lambda row: average_relative_prediction(row, indicator, year), axis=1).apply(pd.Series)
+        try :
+            df.to_csv(f'./output/{indicator}_{year}_nuts_llm_{model_short_name}_relative.csv')
+        except:
+            df.to_csv(f'./output/{indicator}_{year}_nuts_llm_error_on_name.csv')
