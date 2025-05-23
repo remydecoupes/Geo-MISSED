fd39078273f5863252f015b8001a9ef80e72ca1a
Ubuntu <remy.decoupes@gmail.com>
2025-05-23 08:23:19 +0000

    re-work geo stat for abstract

--- 0_inspect_geo-information_std_benchmarks.py
+++ 0_inspect_geo-information_std_benchmarks.py
@@ -49,6 +49,7 @@ benchmarks = [hellaswag_dict, openbookqa_dict, truthfulqa_dict]
 
 # MMLU
 # https://huggingface.co/datasets/cais/mmlu
+## only geographic subject
 mmlu_subjects = [
     "high_school_geography",
     "high_school_world_history",
@@ -57,6 +58,23 @@ mmlu_subjects = [
     "international_law",
     "global_facts",
     "prehistory"
+]
+## all subjects: 57
+mmlu_subjects = [
+    "abstract_algebra", "anatomy", "astronomy", "business_ethics", "clinical_knowledge",
+    "college_biology", "college_chemistry", "college_computer_science", "college_mathematics",
+    "college_medicine", "college_physics", "computer_security", "conceptual_physics",
+    "econometrics", "electrical_engineering", "elementary_mathematics", "formal_logic",
+    "global_facts", "high_school_biology", "high_school_chemistry", "high_school_computer_science",
+    "high_school_european_history", "high_school_geography", "high_school_government_and_politics",
+    "high_school_macroeconomics", "high_school_mathematics", "high_school_microeconomics",
+    "high_school_physics", "high_school_psychology", "high_school_statistics",
+    "high_school_us_history", "high_school_world_history", "human_aging", "human_sexuality",
+    "international_law", "jurisprudence", "logical_fallacies", "machine_learning", "management",
+    "marketing", "medical_genetics", "miscellaneous", "moral_disputes", "moral_scenarios",
+    "nutrition", "philosophy", "prehistory", "professional_accounting", "professional_law",
+    "professional_medicine", "professional_psychology", "public_relations", "security_studies",
+    "sociology", "us_foreign_policy", "virology", "world_religions"
 ]
 
 mmlu_subject = {}
@@ -143,6 +161,9 @@ datasets_to_plot = ["hellaswag", "openbookqa", "truthfulqa", "mmlu"]
 df_filtered = df_benchmarks[df_benchmarks["dataset"].isin(datasets_to_plot)]
 
 colors = ["blue", "orange", "green", "red"]
+# pastel
+colors = ["#AEC6CF", "#FFDAB9", "#B0E0E6", "#FFB6C1"]
+
 plt.figure(figsize=(8, 6))
 sns.barplot(
     x="dataset",
@@ -156,7 +177,7 @@ plt.ylabel("Geo Info Percentage (%)", fontsize=12)
 plt.xticks(rotation=45)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
-plt.savefig("output/geo-info-distribution-common-benchmark.png")
+plt.savefig("figs/geo-info-distribution-common-benchmark.png")
 
 nlp_aggragate_max = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="max")
 nlp_aggragate_first = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="first")
